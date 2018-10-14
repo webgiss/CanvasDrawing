0334faf82bbb584332c358c2560cd1009bc8ec73
gissehel <public-devgit-common@gissehel.org>
2018-10-14 14:57:38 +0200

    Refactoring on drawTimeTable signature

--- drawTimeTable.js
+++ drawTimeTable.js
@@ -9,27 +9,28 @@ const getPoints = (range) => range.map(i => new Vector(0.98, 2 * Math.PI * i / r
 
 const getIndex = (points, index) => points[(((Math.floor(index)) % points.length) + points.length) % points.length]
 
-const drawTimeTable = (drawing, n, k, m, params) => {
-    let {
-        clear
-    } = params || {};
+const drawTimeTable = (drawing, n, k, {clear, repeat}) => {
     if (m === undefined) {
         m = 1;
     }
     let range = getRange(n);
     let points = getPoints(range);
 
+    if (repeat === undefined) {
+        repeat = 1;
+    }
     if (clear === undefined || clear) {
         drawing.clear();
     }
-    getRange(m)
-        .map((z) => points
-            .map((point, i) => drawing.line({
+    getRange(repeat).map((repeatIndex) => 
+        points.map((point, i) => 
+            drawing.line({
                 point0: point,
-                point1: getIndex(points, k * (i + z * points.length)),
+                point1: getIndex(points, k * (i + repeatIndex * points.length)),
                 color: '#331118'
-            }))
-        );
+            })
+        )
+    );
 }
 
 window.CanvasDrawing = { ...(window.CanvasDrawing || {}), getRange, getIndex, drawTimeTable };
