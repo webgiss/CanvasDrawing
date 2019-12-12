c76bc31561139cffaba8dd9125623d431c4fb62d
jgi <public-vs2015@gissehel.org>
2019-12-12 03:16:44 +0100

    REFACT : Move keymanager at the end of the file

--- crible.js
+++ crible.js
@@ -62,6 +62,110 @@ const keyManager = new KeyManager({
     display_text: false,
 });
 
+const mainAction = (config) => {
+    let ssize = 2 * config.size;
+    let scount = ssize * ssize;
+    let [width, height] = [window.innerWidth, window.innerHeight];
+    let minDim = Math.min(width, height);
+    drawing.reinit({
+        height,
+        width,
+        origin: [width / 2, height / 2],
+        mapx: (minDim / 2) / config.size,
+        mapy: -(minDim / 2) / config.size,
+        bgcolor: background_color,
+    });
+
+    primes.ensureSize(scount, ssize);
+
+    const drawNumber = (k) => {
+        let kk = k - config.spiral_offset;
+        let r = Math.floor(Math.sqrt(kk));
+        let mr = r - (r + 1) % 2;
+        let mmr = mr + 1;
+        let b = mr * mr;
+        let yb = (mr + 1) / 2;
+        let d = kk - b;
+        let decl = d % mmr;
+        let phase = (d - decl) / mmr;
+
+        let [x, y] = [yb, -yb];
+
+        if (phase >= 1) {
+            y = y + mmr;
+        }
+
+        if (phase >= 2) {
+            x = x - mmr;
+        }
+
+        if (phase >= 3) {
+            y = y - mmr;
+        }
+
+        switch (phase) {
+            case 0:
+                y = y + decl + 1;
+                break;
+            case 1:
+                x = x - decl - 1;
+                break;
+            case 2:
+                y = y - decl - 1;
+                break;
+            case 3:
+                x = x + decl + 1;
+                break;
+        }
+
+        if (config.display_non_2_3_multiple) {
+            if (k % 6 === 1 || k % 6 === 5) {
+                drawing.filledRectangle({ point0: new Vector(x, y), point1: new Vector(x + 1, y + 1), color: non_2_3_multiple_color });
+            }
+        }
+
+        if (config.display_prime) {
+            if (primes.isPrime(k)) {
+                drawing.filledRectangle({ point0: new Vector(x, y), point1: new Vector(x + 1, y + 1), color: prime_color });
+            }
+        }
+
+        if (config.display_rectangle) {
+            drawing.rectangle({ point0: new Vector(x, y), point1: new Vector(x + 1, y + 1), color: rectangle_color });
+        }
+
+        if (config.display_text) {
+            drawing.addText({ text: '' + k, color: text_color, fontFamily: 'Calibri', fontSize: text_size + 'px', position: '5', point: new Vector(x + 0.5, y + 0.5) });
+        }
+    }
+
+    for (let index = config.spiral_offset; index < scount; index++) {
+        drawNumber(index);
+    }
+
+    if (config.display_spiral) {
+        let x = 1
+        let y = 1
+        let index = 1
+
+        while (index <= ssize) {
+            drawing.line({ point0: [x, y], point1: [x - index, y], color: spiral_color })
+            x = x - index
+            drawing.line({ point0: [x, y], point1: [x, y - index], color: spiral_color })
+            y = y - index
+
+            index = index + 1
+
+            drawing.line({ point0: [x, y], point1: [x + index, y], color: spiral_color })
+            x = x + index
+            drawing.line({ point0: [x, y], point1: [x, y + index], color: spiral_color })
+            y = y + index
+
+            index = index + 1
+        }
+    }
+};
+
 keyManager
     .add("ArrowUp", config => config.spiral_offset += 1)
     .add("ArrowDown", config => config.spiral_offset -= 1)
@@ -70,113 +174,11 @@ keyManager
     .add("+", config => config.size += 1)
     .add("-", config => config.size -= 1)
     .add("*", config => config.display_non_2_3_multiple = !config.display_non_2_3_multiple)
-    .add("ù", config => config.display_prime = !config.display_prime)
+    .add("\u00f9", config => config.display_prime = !config.display_prime)
     .add("r", config => config.display_rectangle = !config.display_rectangle)
     .add("s", config => config.display_spiral = !config.display_spiral)
     .add("t", config => config.display_text = !config.display_text)
     .add('x', config => body.classList.swap('maxwidth'))
     .add('y', config => body.classList.swap('maxheight'))
-    .setAction(config => {
-        let ssize = 2 * config.size;
-        let scount = ssize * ssize;
-        let [width, height] = [window.innerWidth, window.innerHeight];
-        let minDim = Math.min(width, height);
-        drawing.reinit({
-            height,
-            width,
-            origin: [width / 2, height / 2],
-            mapx: (minDim / 2) / config.size,
-            mapy: -(minDim / 2) / config.size,
-            bgcolor: background_color,
-        });
-
-        primes.ensureSize(scount, ssize);
-
-        const drawNumber = (k) => {
-            let kk = k - config.spiral_offset;
-            let r = Math.floor(Math.sqrt(kk));
-            let mr = r - (r + 1) % 2;
-            let mmr = mr + 1;
-            let b = mr * mr;
-            let yb = (mr + 1) / 2;
-            let d = kk - b;
-            let decl = d % mmr;
-            let phase = (d - decl) / mmr;
-
-            let [x, y] = [yb, -yb];
-
-            if (phase >= 1) {
-                y = y + mmr;
-            }
-
-            if (phase >= 2) {
-                x = x - mmr;
-            }
-
-            if (phase >= 3) {
-                y = y - mmr;
-            }
-
-            switch (phase) {
-                case 0:
-                    y = y + decl + 1;
-                    break;
-                case 1:
-                    x = x - decl - 1;
-                    break;
-                case 2:
-                    y = y - decl - 1;
-                    break;
-                case 3:
-                    x = x + decl + 1;
-                    break;
-            }
-
-            if (config.display_non_2_3_multiple) {
-                if (k % 6 === 1 || k % 6 === 5) {
-                    drawing.filledRectangle({ point0: new Vector(x, y), point1: new Vector(x + 1, y + 1), color: non_2_3_multiple_color });
-                }
-            }
-
-            if (config.display_prime) {
-                if (primes.isPrime(k)) {
-                    drawing.filledRectangle({ point0: new Vector(x, y), point1: new Vector(x + 1, y + 1), color: prime_color });
-                }
-            }
-
-            if (config.display_rectangle) {
-                drawing.rectangle({ point0: new Vector(x, y), point1: new Vector(x + 1, y + 1), color: rectangle_color });
-            }
-
-            if (config.display_text) {
-                drawing.addText({ text: '' + k, color: text_color, fontFamily: 'Calibri', fontSize: text_size + 'px', position: '5', point: new Vector(x + 0.5, y + 0.5) });
-            }
-        }
-
-        for (let index = config.spiral_offset; index < scount; index++) {
-            drawNumber(index);
-        }
-
-        if (config.display_spiral) {
-            let x = 1
-            let y = 1
-            let index = 1
-
-            while (index <= ssize) {
-                drawing.line({ point0: [x, y], point1: [x - index, y], color: spiral_color })
-                x = x - index
-                drawing.line({ point0: [x, y], point1: [x, y - index], color: spiral_color })
-                y = y - index
-
-                index = index + 1
-
-                drawing.line({ point0: [x, y], point1: [x + index, y], color: spiral_color })
-                x = x + index
-                drawing.line({ point0: [x, y], point1: [x, y + index], color: spiral_color })
-                y = y + index
-
-                index = index + 1
-            }
-        }
-    })
-    ;
+    .setAction(mainAction)
+;
